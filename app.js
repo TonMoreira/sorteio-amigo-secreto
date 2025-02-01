6e1567ac4da5979ae25707512629a6c7ef994cac
Ewerton <tonmoreira7@gmail.com>
2025-02-01 00:50:55 -0300

    Versão 1.0 - Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigoSecreto = document.getElementById("amigo").value;
+
+    if (amigoSecreto === "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(amigoSecreto);
+        limparCampo();
+        atualizarLista();
+    }
+}
+
+
+
+function limparCampo() {
+    let amigoSecreto = document.getElementById("amigo");
+    amigoSecreto.value = "";
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li"); // Cria um <li>
+        item.textContent = amigos[i]; // Define o texto do <li>
+        lista.appendChild(item); // Adiciona o <li> à <ul>
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um amigo antes de sortear!");
+        return;
+    }
+
+    let indiceSorteado = Math.floor(Math.random() * amigos.length); // Gera um índice aleatório
+    let amigoSorteado = amigos[indiceSorteado]; // Obtém o nome sorteado
+
+    document.getElementById("resultado").innerHTML = `🎉 O amigo sorteado foi: <strong>${amigoSorteado}</strong>!`;
+}
